46baf0c84798337dc78f5a627e501ea332b4ac3c
Nejer <Nejer6@gmail.com>
2022-11-12 22:15:09 +0300

    initial commit

---
+++ src/main/kotlin/com/nejer/plugins/Routing.kt
@@ -0,0 +1,20 @@
+package com.nejer.plugins
+
+import io.ktor.server.routing.*
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.request.*
+
+fun Application.configureRouting() {
+
+    routing {
+        route("api") {
+            route("v1") {
+                get {
+                    call.respondText("Hello World!")
+                }
+            }
+        }
+    }
+}
